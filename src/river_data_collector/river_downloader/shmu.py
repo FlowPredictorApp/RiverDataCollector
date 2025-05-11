9caa0ea517b3dd56a96a159bfa77bb8cdba376f0
GasiorMateusz <mateuszgasior314@gmail.com>
2025-05-11 16:30:28 +0200

    refactor shmu

--- src/river_data_collector/river_downloader/shmu.py
+++ src/river_data_collector/river_downloader/shmu.py
@@ -8,18 +8,18 @@ from river_data_collector.river_downloader.river_downloader_interface import Riv
 class SlovakiaRiverNames:
     JAVORINKA = "Javorinka"
 
-class Shmu(RiverDownloaderInterface):
+class ShmuDownloader(RiverDownloaderInterface):
 
-    url = "https://www.shmu.sk/en/?page=1&id=hydro_vod_all&station_id=7930"
-    
+    BASE_URL = "https://www.shmu.sk/en/?page=1&id=hydro_vod_all&station_id=7930"
+
     def get_river_data(self, river_name: str) -> Optional[List[Dict]]:
         if river_name == SlovakiaRiverNames.JAVORINKA:
-            return self.get_javorinka()
+            return self.get_javorinka_data()
         else:
             print(f"River {river_name} not supported.")
-            print (f"Supported rivers: {', '.join([name for name in SlovakiaRiverNames.__dict__.keys() if not name.startswith('__')])}")
+            print(f"Supported rivers: {', '.join([name for name in SlovakiaRiverNames.__dict__.keys() if not name.startswith('__')])}")
             return None
-        
+
     def parse_table(self, table: BeautifulSoup) -> Optional[List[Dict]]:
         rows = []
         for row in table.find_all("tr"):
@@ -36,32 +36,36 @@ class Shmu(RiverDownloaderInterface):
         else:
             print("Table is empty")
             return None
-        
+
     def get_service_data(self, url: str) -> requests.Response:
-        
         headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(url, headers=headers)
-        response.encoding = 'utf-8'
-        return response
-
-    def get_javorinka(self) -> Optional[List[Dict]]:
-        
-        response = self.get_service_data(self.url)
-        if response.status_code != 200:
-            print(f"Error fetching data: {response.status_code}")
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            response.encoding = 'utf-8'
+            return response
+        except requests.RequestException as error:
+            print(f"Request failed: {error}")
             return None
-        soup = BeautifulSoup(response.text, "html.parser")
-        table: Optional[BeautifulSoup] = soup.find("table", class_="dynamictable w600 center stripped")
 
-        if table is not None:
-            return self.parse_table(table)
+    def get_javorinka_data(self) -> Optional[List[Dict]]:
+        response = self.get_service_data(self.BASE_URL)
+        if response and response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            table: Optional[BeautifulSoup] = soup.find("table", class_="dynamictable w600 center stripped")
+
+            if table is not None:
+                return self.parse_table(table)
+            else:
+                print("Table not found")
+                return None
         else:
-            print("Table not found")
+            print(f"Error fetching data: {response.status_code if response else 'No response'}")
             return None
-        
+
 if __name__ == "__main__":
-    shmu = Shmu()
-    data = shmu.get_river_data(SlovakiaRiverNames.JAVORINKA)
+    downloader = ShmuDownloader()
+    data = downloader.get_river_data(SlovakiaRiverNames.JAVORINKA)
     if data:
         for entry in data:
             print(entry)
